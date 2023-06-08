b5357014c9a78432b63aac07c87ba276ce90713d
nmke-de <github@nmke.de>
2023-06-08 15:09:22 +0200

    Implemented loq_add()

--- loq_add.c
+++ loq_add.c
@@ -1,8 +1,19 @@
-#include <unistd.h>
+#include <fcntl.h>
 #include <string.h>
+#include <time.h>
+#include <unistd.h>
+#include "Itoa/itoa.h"
 #include "loq.h"
 
+#define fdprint(fd, str) write((fd), (str), strlen((str)))
+
 void loq_add(char *msg) {
-	write(1, msg, strlen(msg));
-	// TODO implement
+	// TODO Create file on first write
+	time_t now = time(NULL);
+	int fd = open("testloq", O_WRONLY | O_APPEND);
+	fdprint(fd, itoa(now, 10));
+	fdprint(fd, "\t");
+	fdprint(fd, msg);
+	fdprint(fd, "\n");
+	close(fd);
 }
