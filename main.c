bbd28ed9899b4af83f8379ddf7ac3ef2a94b5ed9
nmke-de <github@nmke.de>
2023-06-08 13:25:32 +0200

    Fixed msg parsing

--- main.c
+++ main.c
@@ -10,18 +10,18 @@ int main(int argc, char **argv) {
 	else {
 		// Parse arguments to message, with each argument separated by space.
 		// Allocate memory
-		int len = argc;
+		int len = argc - 1;
 		int *lens = alloca(sizeof(int) * (argc - 1));
 		for (int i = 1; i < argc; i++) {
-			lens[i - 1] += strlen(argv[i]);
+			lens[i - 1] = strlen(argv[i]);
 			len += lens[i - 1];
 		}
 		char *msg = alloca(sizeof(char) * len);
 		// Copy arguments to msg
-		for (int i = 1, l = 0; i < argc; i++, l++) {
+		for (int i = 1, l = 0; i < argc; i++) {
 			strncpy(msg + l, argv[i], lens[i - 1]);
 			l += lens[i - 1];
-			msg[++l] = ' ';
+			msg[l++] = ' ';
 		}
 		msg[len - 1] = '\0';
 		loq_add(msg);
